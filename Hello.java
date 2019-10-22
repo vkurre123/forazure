af80ef42b730a227c10c51edf6b157b041f90b78
venu <kvenu.reddy1@googlemail.com>
2019-10-22 07:03:59 +0530

    new files added

---
+++ Hello.java
@@ -0,0 +1,17 @@
+public class Hello{
+  public static void main(String[] args){
+
+
+
+      for(int i=0;i<=2;i++)
+         {
+             System.out.println("venu");
+         }
+
+
+  }
+
+
+
+
+}
